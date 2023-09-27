d96c54653b7cdb45f613dad7942e8cb2d0aff954
Murugankaliyankvm <murugankaliyankvm@gmail.com>
2023-09-27 17:39:59 +0530

    hero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
-import Intro from './components/Intro/Intro';
-import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import About from './components/about/About';
+import Footer from './components/common/footer/Footer';
+import Contact from './components/contact/Contact';
+import Header from './components/header/Header';
+
+import {
+  BrowserRouter as Router,
+  Switch,Route
+
+} from "react-router-dom";
+import { team } from './dummydata';
 
 function App() {
   return (
-    <div className="App">
-        <Navbar/>
-        <Intro/>
-    </div>
+    <>         
+       <Router>     
+        <Header/>          
+              <Switch>
+                  <Route exact path='/' component={Home} />            
+                  <Route exact path='/about' component={About} />            
+                
+                  
+                  <Route exact path='/contact' component={Contact} />            
+         
+              </Switch>
+              <Footer/>
+        </Router>
+    </>
   );
 }
 
